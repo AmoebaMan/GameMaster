574e83b17e94670b0e2d0c9065e7b00e19a9a915
Dennison Richter <drichter@amoebaman.net>
2014-03-12 16:02:39 -0400

    Javadocs

--- src/net/amoebaman/gamemaster/api/AutoGame.java
+++ src/net/amoebaman/gamemaster/api/AutoGame.java
@@ -9,72 +9,98 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Represents a game that can be run automatically using the GameMaster API.
+ * 
+ * @author AmoebaMan
+ */
 public abstract class AutoGame extends JavaPlugin{
 	
 	/**
-	 * Gets the primary name of the game, for displaying purposes.
-	 * This is drawn directly from the plugin name, replacing all underscores and dashes with spaces.
-	 * @return
+	 * Gets the primary name of the game, for displaying purposes. This is drawn
+	 * directly from the plugin name, replacing all underscores and dashes with
+	 * spaces.
+	 * 
+	 * @return the name of the game
 	 */
 	public String getGameName(){
 		return getDescription().getName().replace('-', ' ').replace('_', ' ');
 	}
 	
+	/**
+	 * We return {@link #getName()} so that auto games can be inserted directly
+	 * into string literals for nice-looking results.
+	 */
 	public String toString(){
 		return getGameName();
 	}
 	
 	/**
-	 * Checks to see if the game is active, that is, that it is playing actively.
-	 * This should be checked prior to ALL event handling, so that events aren't handled in unexpected ways by inactive events.
-	 * @return
+	 * Checks to see if the game is active, that is, that it is playing
+	 * actively. This should be checked prior to ALL event handling, so that
+	 * events aren't handled in unexpected ways by inactive events.
+	 * 
+	 * @return true if the game is running, false otherwise
 	 */
 	public boolean isActive(){
 		return GameMaster.status == MasterStatus.RUNNING && GameMaster.activeGame.equals(this);
 	}
 	
 	/**
-	 * Gets an array containing the aliases by which this game can be referred to.
-	 * These are used as shortcut names for things like game voting.
-	 * @return the aliases this game should respond to
+	 * Gets an array containing the aliases by which this game can be referred
+	 * to. These are used as shortcut names for things like game voting.
+	 * 
+	 * @return the game's aliases
 	 */
 	public abstract String[] getAliases();
 	
 	/**
-	 * Checks whether a GameMap is compatible with this game.
-	 * GameMaps are designed to be universal and share all their data between games with each game using what it needs.
-	 * This method should check to see that all the data the game needs is present in the properties of the map, and that the game can run safely.
-	 * @param map the map to check.
-	 * @return true if this game can run without error on the map, false otherwise
+	 * Checks whether a {@link GameMap} is compatible with this game.
+	 * GameMaps are designed to be universal and share all their data between
+	 * games with each game using what it needs. This method should check to see
+	 * that all the data the game needs is present in the properties of the map,
+	 * and that the game can run safely.
+	 * 
+	 * @param map a map
+	 * @return true if this game can run without error on the map, false
+	 *         otherwise
 	 */
 	public abstract boolean isCompatible(GameMap map);
 	
 	/**
-	 * Gets the color that should be appended to a player's name.
-	 * This is called periodically by the GameMaster to keep player's colors up to date.
-	 * Returning null will cause all color to be removed from the player's name.
-	 * @param player the player
+	 * Gets the color that should be appended to a player's name while this game
+	 * is running. This is called periodically by the GameMaster to keep
+	 * player's colors up to date. Returning null will cause all color to be
+	 * removed from the player's name.
+	 * 
+	 * @param player a player
 	 * @return the color the player's name should be
 	 */
 	public abstract ChatColor getNameColor(Player player);
 	
 	/**
-	 * Gets a list of Strings to be sent to the player when they perform the /game command, requesting the status of the game.
-	 * This should include score, time remaining, etc., all in a readable form.
+	 * Gets a list of Strings to be sent to the player when they perform the
+	 * /game command, requesting the status of the game. This should include
+	 * score, time remaining, etc., all in a readable form.
+	 * 
 	 * @param player the player this information will be sent to
 	 * @return the status of the game
 	 */
 	public abstract List<String> getStatus(Player player);
 	
 	/**
-	 * Safely adds a player to the game.
-	 * @param player the player
+	 * Safely adds a player to the game, such as when they log in, or use the
+	 * {@code /enter} command.
+	 * 
+	 * @param player a player
 	 */
 	public abstract void addPlayer(Player player);
 	
 	/**
-	 * Safely removes a player from the game.
-	 * @param player the player
+	 * Safely removes a player from the game, such as when they log off, or use
+	 * the {@code /exit} command.
+	 * 
+	 * @param player a player
 	 */
 	public abstract void removePlayer(Player player);
 	
--- src/net/amoebaman/gamemaster/api/GameMap.java
+++ src/net/amoebaman/gamemaster/api/GameMap.java
@@ -4,23 +4,34 @@ import org.bukkit.Bukkit;
 
 import net.amoebaman.gamemaster.utils.PropertySet;
 
-public class GameMap {
+/**
+ * Represents a map for {@link AutoGame}{@code s} to run on. Maps are designed
+ * to be universal - all their data is stored in a properties map, and each game
+ * should draw on whatever it requires to run. Games check for map compatibility
+ * with {@link AutoGame#isCompatible(GameMap)}.
+ * 
+ * @author AmoebaMan
+ */
+public class GameMap{
 	
 	/** The name of the map */
 	public final String name;
-	/** Contains all the values of the map in one place, to allow easy cross-compatibility */
+	
+	/**
+	 * Contains all the values of the map in one place, to allow easy
+	 * cross-compatibility
+	 */
 	public final PropertySet properties;
 	
 	/**
-	 * Creates a new ArenaMap with a specified name and no properties except those requried.
-	 * Arena maps cannot contain spaces in their names.
+	 * Creates a new ArenaMap with a specified name and no properties except
+	 * those requried by default. Game maps cannot contain spaces in their
+	 * names, so all spaces will be removed from the name.
+	 * 
 	 * @param name the name of the map
-	 * @throws IllegalArgumentException if a space is present in name
 	 */
 	public GameMap(String name) throws IllegalArgumentException{
-		if(name.contains(" "))
-			throw new IllegalArgumentException("map names cannot contain spaces");
-		this.name = name;
+		this.name = name.replace(" ", "");
 		properties = new PropertySet();
 		properties.options().pathSeparator('/');
 		/*
@@ -29,8 +40,18 @@ public class GameMap {
 		properties.set("world", Bukkit.getWorlds().get(0).getName());
 	}
 	
-	public String toString(){ return name; }
+	/**
+	 * We return {@link #name} so that game maps can be inserted directly
+	 * into string literals for nice-looking results.
+	 */
+	public String toString(){
+		return name;
+	}
 	
+	/**
+	 * Maps check for equality based on their name, to avoid maps with duplicate
+	 * names.
+	 */
 	public boolean equals(Object x){
 		if(x instanceof GameMap)
 			return toString().equalsIgnoreCase(x.toString());
@@ -38,6 +59,10 @@ public class GameMap {
 			return false;
 	}
 	
+	/**
+	 * Maps hash themselves based on their name, to avoid maps with duplicate
+	 * names.
+	 */
 	public int hashCode(){
 		return toString().hashCode();
 	}
